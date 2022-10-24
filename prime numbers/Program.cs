2d51d992beb3cfab020ffed460d56c273f6932ad
Somber <dj2548@gmail.com>
2022-10-24 17:04:02 -0400

    poop

---
+++ prime numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
